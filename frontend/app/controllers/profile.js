267546a7a131039b0363449edd79b4806b892961
Aamir Mahmood <aamir@aalasolutions.com>
2026-03-16 12:07:19 +0500

    fix: remove email from profile update (not accepted by DTO)

--- frontend/app/controllers/profile.js
+++ frontend/app/controllers/profile.js
@@ -31,7 +31,6 @@ export default class ProfileController extends Controller {
     try {
       const body = {
         name: this.formName,
-        email: this.formEmail,
       };
 
       if (this.formPassword) {
