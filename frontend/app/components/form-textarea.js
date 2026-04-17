e80a650c83e51c8c82b1bf276ebfb4c337a45d3c
mbilal1007 <m.bilal@aalasolutions.com>
2026-04-17 15:22:30 +0500

    Enhance form components with additional attributes and improve layout styles

--- frontend/app/components/form-textarea.js
+++
@@ -1,5 +0,0 @@
-import Component from '@glimmer/component';
-
-export default class FormTextareaComponent extends Component {
-  // No additional logic needed - this is a presentational component
-}
