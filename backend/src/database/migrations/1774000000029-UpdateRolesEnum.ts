6d96978ca3dbc03dc0ce21cfef3b28ab7d74e3de
kamran <kamran@aalasolutions.com>
2026-05-13 17:29:47 +0500

    completed feedback

--- backend/src/database/migrations/1774000000029-UpdateRolesEnum.ts
+++ backend/src/database/migrations/1774000000029-UpdateRolesEnum.ts
@@ -1,4 +1,4 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class UpdateRolesEnum1774000000029 implements MigrationInterface {
     name = 'UpdateRolesEnum1774000000029';
@@ -13,5 +13,6 @@ export class UpdateRolesEnum1774000000029 implements MigrationInterface {
         // PostgreSQL does not support removing enum values
         // If this migration needs to be reverted, the app version
         // should be compatible with the 6-value enum.
+        void queryRunner;
     }
 }
