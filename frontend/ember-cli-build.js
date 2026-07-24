f21c6db8c4663f2471bb9a5e494cee78b2537242
Aamir Mahmood <aamir@aalasolutions.com>
2026-07-24 12:26:06 +0500

    fingerprint improved

--- frontend/ember-cli-build.js
+++ frontend/ember-cli-build.js
@@ -4,6 +4,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
+    fingerprint: {
+      exclude: ['logo.png', 'icons/*'], // Exclude specific files or paths
+    },
     emberData: {
       deprecations: {
         // New projects can safely leave this deprecation disabled.
